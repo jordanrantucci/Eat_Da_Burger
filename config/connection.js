bdd0b306d4b839b2f65872c657ba371980c5ce81
Jordan Rantucci <jordanrantucci@gmail.com>
2021-02-24 22:47:00 -0500

    got everything connected and working with cats, I will have to reconfigure everything for burgers

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   // NOTE: Be sure to add your MySQL password here!
-  password: '',
+  password: 'ThinkPad$1989',
   database: 'cat_db',
 });
 
